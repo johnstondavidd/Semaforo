cc63545c27c2e4f22430ac70be71eca5b59081c7
joaquinvoeffray <voeffrayjoaco@gmail.com>
2020-04-24 17:32:03 -0300

    3seg led prendidos por boton accionado Voeffray

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+#include<Arduino.h>      //marca un herror 
 
 const int red = PIN0;
 const int yellow = PIN1;
@@ -15,10 +15,18 @@ void setup()
   pinMode(button, INPUT_PULLUP);
 }
 
+
 void loop()
 {
   if (digitalRead(button))
   {
+    digiralWrite(green, HIGH);    //agregado para comit de Joaco V
+    digiralWrite(red, HIGH);      //si el boton esta presionado prende todas las luces 3 seg y las apaga. 
+    digiralWrite(yellow, HIGH);
+    delay(SECONDS(3));
+    digiralWrite(green, LOW);
+    digiralWrite(red, LOW);
+    digiralWrite(yellow, LOW);
 //agregado por guillermo para testear si se puede comitear al remoto
   }
   digitalWrite(green, LOW);
